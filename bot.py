a74ccfca6bdf49e1eaede9fe833b7ddea6ee9b7e
denisfedko657-sys <denisfedko657@gmail.com>
2026-07-14 17:59:06 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
-import asyncio
+mport asyncio
+import re
 from aiogram import Bot, Dispatcher
 from aiogram.filters import CommandStart
 from aiogram.types import Message
@@ -7,19 +8,55 @@ bot = Bot("8459389604:AAHfjNynnxJeyyty7RvLuPkJB5j7vkTwhc0")
 dp = Dispatcher()
 
 
+# Заборонені слова
+bad_words = [
+    "smith", "johnson", "jackson",
+    "killer", "shadow", "dragon",
+    "admin", "owner", "boss",
+    "228", "1337"
+]
+
+
 @dp.message(CommandStart())
 async def start(message: Message):
     await message.answer(
-        "👋 Вітаю!\n\n"
-        "Надішли мені RP NickName для перевірки.\n\n"
-        "Наприклад:\n"
-        "Денис Дасслер"
+        "Напиши NickName для перевірки RP.\n\n"
+        "Приклад: Денис Дасслер"
     )
 
 
 @dp.message()
 async def check(message: Message):
-    await message.answer(f"🔍 Перевіряю:\n{message.text}")
+    nickname = message.text.strip()
+    lower = nickname.lower()
+
+    # Перевірка заборонених слів
+    for word in bad_words:
+        if word in lower:
+            await message.answer(
+                f"❌ {nickname}\n"
+                "Не RP\n"
+                "Причина: заборонене слово або іноземний нік"
+            )
+            return
+
+    # Формат Ім'я Прізвище
+    if not re.fullmatch(
+        r"[А-ЯІЇЄҐ][а-яіїєґ]+\s[А-ЯІЇЄҐ][а-яіїєґ]+",
+        nickname
+    ):
+        await message.answer(
+            f"❌ {nickname}\n"
+            "Не RP\n"
+            "Причина: неправильний формат"
+        )
+        return
+
+
+    await message.answer(
+        f"✅ {nickname}\n"
+        "RP NickName"
+    )
 
 
 async def main():
