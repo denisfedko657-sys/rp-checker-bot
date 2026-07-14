b82969f70303198e4a329efe26ac7594d7548db5
denisfedko657-sys <denisfedko657@gmail.com>
2026-07-14 16:54:34 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,32 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+from aiogram.filters import CommandStart
+from aiogram.types import Message
 
+from config import BOT_TOKEN
+
+bot = Bot(BOT_TOKEN)
+dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def start(message: Message):
+    await message.answer(
+        "👋 Вітаю!\n\n"
+        "Надішли мені RP NickName для перевірки.\n\n"
+        "Наприклад:\n"
+        "Денис Дасслер"
+    )
+
+
+@dp.message()
+async def check(message: Message):
+    await message.answer(f"🔍 Перевіряю:\n{message.text}")
+
+
+async def main():
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
