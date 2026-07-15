83617ff8e05639f666d6769dbd502b008ab64146
denisfedko657-sys <denisfedko657@gmail.com>
2026-07-15 13:28:38 +0300

    Update bot.py

--- nickname_bot/bot.py
+++ nickname_bot/bot.py
@@ -1,1 +1,35 @@
+from aiogram import Bot, Dispatcher
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+from aiogram import F
 
+from config import BOT_TOKEN
+from validator import validate_format
+
+bot = Bot(BOT_TOKEN)
+dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def start(message: Message):
+    await message.answer(
+        "👋 Вітаю!\n\nНадішли мені нікнейм у форматі:\n\nІм'я Прізвище"
+    )
+
+
+@dp.message(F.text)
+async def check(message: Message):
+
+    if validate_format(message.text):
+        await message.answer("✅ Формат правильний")
+    else:
+        await message.answer("❌ Неправильний формат")
+
+
+async def main():
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    import asyncio
+    asyncio.run(main())
