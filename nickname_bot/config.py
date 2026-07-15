fe1a2e5f1241d9cc896e31ebb0155136e1e99f0f
denisfedko657-sys <denisfedko657@gmail.com>
2026-07-15 14:17:48 +0300

    Update config.py

--- nickname_bot/config.py
+++ nickname_bot/config.py
@@ -3,4 +3,4 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("8459389604:AAHfjNynnxJeyyty7RvLuPkJB5j7vkTwhc0")
+BOT_TOKEN = os.getenv("8459389604:AAH9vHA0l-wPCoQRjahGcDMQJNohE33wumQ")
